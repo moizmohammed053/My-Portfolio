59c909ab6ac5e0b02b9dd1862356ca9b3ed7e4c1
Mohd.Moiz Ahmed <mohd.moiz@outlook.com>
2024-12-24 13:52:16 -0500

    first commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,14 @@
+import { Html } from "@react-three/drei"
+
+const Loader = () => {
+  return (
+    <HTML>
+        <div className="flex justify-center items-center">
+        <div className='w-20 h-20 border-opacity-20 border-blue-500 boarder-t-blue-500 rounded-full animate-spin'/>
+    </div>
+    </HTML>
+    
+  )
+}
+
+export default Loader
