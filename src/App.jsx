3e30abac26e2ab0602103fd1ba97e7662dde7798
Mohd.Moiz Ahmed <mohd.moiz@outlook.com>
2024-12-24 00:18:11 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={'Home'} />
+          <Route path="/" element={'About'} />
+          <Route path="/" element={'Projects'} />
+          <Route path="/" element={'Contact'} />
+        </Routes>
+      </Router>
+    </main>
+  );
+};
 
-export default App
+export default App;
