345a052bd6aeae006815ec0b30beab32d921b1ea
Mohd.Moiz Ahmed <mohd.moiz@outlook.com>
2024-12-24 15:01:46 -0500

    first commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,29 +1,58 @@
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
-import { Suspense } from 'react'
-import Loader from '../components/Loader'
-{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-            POPUP
-        </div> */}
+// import React from 'react'
+// import { Canvas } from '@react-three/fiber'
+// import { Suspense } from 'react'
+// import Loader from '../components/Loader'
+// import Island  from '../Models/Island'
+// {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+//             POPUP
+//         </div> */}
+// const Home = () => {
+//   return (
+//     <section className="w-full h-screen relative">
+//         <canvas 
+//         className="w-full h-screen bg-transparent" 
+//         camera={{ near:0.1, far:1000}}
+//         >
+// <Suspense fallback={<Loader />}>
+// <directionalLight />
+// <ambientLight />
+// < pointLight />
+// <spotLight />
+// <hemisphereLight />
+// <Island />
+// </Suspense>
+//         </canvas>
+
+//     </section>
+//   )
+// }
+
+// export default Home
+
+
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import Loader from '../components/Loader';
+import Island from '../Models/Island';
+
 const Home = () => {
   return (
     <section className="w-full h-screen relative">
-        <canvas 
+      <Canvas 
         className="w-full h-screen bg-transparent" 
-        camera={{ near:0.1, far:1000}}
-        >
-<Suspense fallback={<Loader />}>
-<directionalLight />
-<ambientLight />
-< pointLight />
-<spotLight />
-<hemisphereLight />
-
-</Suspense>
-        </canvas>
-
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight  />
+          <ambientLight  />
+          <pointLight  />
+          <spotLight />
+          <hemisphereLight />
+          <Island />
+        </Suspense>
+      </Canvas>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,14 +1,28 @@
-import { Html } from "@react-three/drei"
+// import { html } from "@react-three/drei"
+
+// const Loader = () => {
+//   return (
+//     <html>
+//         <div className="flex justify-center items-center">
+//         <div className='w-20 h-20 border-opacity-20 border-blue-500 boarder-t-blue-500 rounded-full animate-spin'/>
+//     </div>
+//     </html>
+    
+//   )
+// }
+
+// export default Loader
+
+import { Html } from '@react-three/drei';
 
 const Loader = () => {
   return (
-    <HTML>
-        <div className="flex justify-center items-center">
-        <div className='w-20 h-20 border-opacity-20 border-blue-500 boarder-t-blue-500 rounded-full animate-spin'/>
-    </div>
-    </HTML>
-    
-  )
-}
+    <Html center>
+      <div className="flex justify-center items-center">
+        <div className="w-20 h-20 border-4 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin" />
+      </div>
+    </Html>
+  );
+};
 
-export default Loader
+export default Loader;
