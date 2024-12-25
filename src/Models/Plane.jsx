bac2c7143693db34b88f6701420eaff8110d492d
Mohd.Moiz Ahmed <mohd.moiz@outlook.com>
2024-12-25 13:14:57 -0500

    first commit

--- src/Models/Plane.jsx
+++ src/Models/Plane.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import planeScene from '../assets/3d/Plane.glb'
-import { useGLTF } from '@react-three/drei'
-const Plane =  ({isRotating, ...props}) => {
+import { useAnimations, useGLTF } from '@react-three/drei'
 
+
+    const Plane =  ({isRotating, ...props}) => {
     const {scene, animations } = useGLTF(planeScene)
+    const ref = useRef();
+    const {actions } = useAnimations(animations, ref);
+
+    useEffect(() =>{
+      if(isRotating){
+        
+      }
+
+    }, [actions, isRotating])
   return (
-    <mesh {...props}>
+    <mesh {...props} ref = {ref}>
     < primitive object = {scene}/>
           </mesh>
   )
