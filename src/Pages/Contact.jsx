b9c0bb852d710700c4d2fe92fbbcbb4c93768064
Mohd.Moiz Ahmed <mohd.moiz@outlook.com>
2024-12-26 03:17:36 -0500

    first commit

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,6 +1,8 @@
-import React, { useRef, useState } from 'react';
+import React, { Suspense, useRef, useState } from 'react';
 import emailjs from '@emailjs/browser'
-
+import { Canvas } from '@react-three/fiber';
+import { Fox } from '../Models/Fox';
+import { Loader } from '../components/Loader';
 const Contact = () => {
   const formRef = useRef (null);
   const [form, setForm]= useState({name: '', email:'', message:''})
@@ -71,6 +73,25 @@ const Contact = () => {
 
           <button type='submit' className='btn' onFocus={handleFocus} onBlur={handleBlur}disabled={isLoading}>{isLoading ? 'Sending...' : 'Send Message'}</button>
         </form>
+      </div>
+      <div className='lgw-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+      <Canvas
+      camera={{
+        position:[0, 0, 5],
+        fov:75,
+        near:0.1,
+        far:1000
+      }}
+      >
+      <directionalLight intensity={2.5} position={[0, 0, 1]} />
+        <Suspense fallback={<Loader />}>
+          <Fox
+          position={[ 0.5, 0.35, 0]}
+          rotation={[12, 0, 0]}
+          scale={[0.5, 0.5, 0.5]}
+          />
+        </Suspense>
+      </Canvas>
       </div>
     </section>
   );
