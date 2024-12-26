d34af27a48ef9eacbb25c70d98e5242a78d04e35
Mohd.Moiz Ahmed <mohd.moiz@outlook.com>
2024-12-25 23:52:48 -0500

    first commit

--- src/components/Homeinfo.jsx
+++ src/components/Homeinfo.jsx
@@ -1,27 +1,44 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import {arrow} from '../assets/icons'
+const InfoBox = ({text, link, btnText }) => (
+    <div className='info-box'>
+        <p className="font-medium sm:text-xl text-center">{text}</p>
+        <Link to={link} className=" neo-btn bg-white">
+        {btnText}
+        <img src={arrow} className='w-4 h-4 object-contain'/>
+        </Link>
+    </div>
+)
 const rendererContent ={
     1:(
         <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue 
         py-4 px-8 text-white mx-5'>
-            Hi, I am <span className='font-semibold'>MOIZ MOHAMMED</span>
+            Hi, I am <span className='font-semibold'>MOIZ MOHAMMED </span>👋<br />
+            A <span>Front-End & Fullstack web developer</span>
         </h1>
     ),
     2:(
-        <h1>2</h1>
+        <InfoBox text="I have worked in many languages
+        and developed deep knowladge "
+        link="/about"
+        btnText="Learn more "/> 
     ),
     3:(
-        <h1>3</h1>
+        <InfoBox text="I have worked in many languages
+        and developed deep knowladge "
+        link="/projects"
+        btnText="Visit My Porjects "/> 
     ),
     4:(
-        <h1>4</h1>
+        <InfoBox text="I have worked in many languages
+        and developed deep knowladge "
+        link="/contact"
+        btnText="Let's Talk "/> 
     ),
 }
 
-const InfoBox = ({text, link, btnText })=>(
-    <div>
-        {text}
-    </div>
-)
+
 const Homeinfo = ({currentStage}) => {
   return rendererContent [currentStage] || null;
     
